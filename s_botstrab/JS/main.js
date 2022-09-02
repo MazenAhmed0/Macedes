c78934522e7c3a117d73f7b423f5c4bf459bdec7
Mazen Ahmed <111910113+MazenAhmed0@users.noreply.github.com>
2022-09-02 06:53:00 -0700

    Add files via upload

---
+++ s_botstrab/JS/main.js
@@ -0,0 +1,257 @@
+// start navs ............................................
+function grayMcLaren() {
+    document.getElementById ("McLaren").style.color = "white";
+    document.getElementById ("Lamborghini").style.color = "rgb(93, 90, 90)";
+    document.getElementById ("Mercedes").style.color = "rgb(93, 90, 90)";
+    document.getElementById ("BMW").style.color = "rgb(93, 90, 90)";
+    document.getElementById ("Bugatti").style.color = "rgb(93, 90, 90)";
+    document.getElementById ("Lancer").style.color = "rgb(93, 90, 90)";
+    document.getElementById ("McLaren").style.transition = "1s"
+    document.getElementById ("Lamborghini").style.transition = "1s"
+    document.getElementById ("Mercedes").style.transition = "1s"
+    document.getElementById ("BMW").style.transition = "1s"
+    document.getElementById ("Bugatti").style.transition = "1s"
+    document.getElementById ("Lancer").style.transition = "1s"
+    
+}
+
+
+function levMcLaren() {
+    document.getElementById ("McLaren").style.color = "white";
+    document.getElementById ("Lamborghini").style.color = "white";
+    document.getElementById ("Mercedes").style.color = "white";
+    document.getElementById ("BMW").style.color = "white";
+    document.getElementById ("Bugatti").style.color = "white";
+    document.getElementById ("Lancer").style.color = "white";
+    
+}
+
+function grayLamborghini() {
+  document.getElementById ("McLaren").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lamborghini").style.color = "white";
+  document.getElementById ("Mercedes").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("BMW").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Bugatti").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lancer").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("McLaren").style.transition = "1s"
+  document.getElementById ("Lamborghini").style.transition = "1s"
+  document.getElementById ("Mercedes").style.transition = "1s"
+  document.getElementById ("BMW").style.transition = "1s"
+  document.getElementById ("Bugatti").style.transition = "1s"
+  document.getElementById ("Lancer").style.transition = "1s"
+  
+}
+
+
+
+
+function levLamborghini() {
+  document.getElementById ("McLaren").style.color = "white";
+  document.getElementById ("Lamborghini").style.color = "white";
+  document.getElementById ("Mercedes").style.color = "white";
+  document.getElementById ("BMW").style.color = "white";
+  document.getElementById ("Bugatti").style.color = "white";
+  document.getElementById ("Lancer").style.color = "white";
+  
+}
+
+function grayMercedes() {
+  document.getElementById ("McLaren").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lamborghini").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Mercedes").style.color = "white";
+  document.getElementById ("BMW").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Bugatti").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lancer").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("McLaren").style.transition = "1s"
+  document.getElementById ("Lamborghini").style.transition = "1s"
+  document.getElementById ("Mercedes").style.transition = "1s"
+  document.getElementById ("BMW").style.transition = "1s"
+  document.getElementById ("Bugatti").style.transition = "1s"
+  document.getElementById ("Lancer").style.transition = "1s"
+  
+}
+
+
+
+
+function levMercedes() {
+  document.getElementById ("McLaren").style.color = "white";
+  document.getElementById ("Lamborghini").style.color = "white";
+  document.getElementById ("Mercedes").style.color = "white";
+  document.getElementById ("BMW").style.color = "white";
+  document.getElementById ("Bugatti").style.color = "white";
+  document.getElementById ("Lancer").style.color = "white";
+  
+}
+
+function grayBMW() {
+  document.getElementById ("McLaren").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lamborghini").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Mercedes").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("BMW").style.color = "white";
+  document.getElementById ("Bugatti").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lancer").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("McLaren").style.transition = "1s"
+  document.getElementById ("Lamborghini").style.transition = "1s"
+  document.getElementById ("Mercedes").style.transition = "1s"
+  document.getElementById ("BMW").style.transition = "1s"
+  document.getElementById ("Bugatti").style.transition = "1s"
+  document.getElementById ("Lancer").style.transition = "1s"
+  
+}
+
+
+
+
+function levBMW() {
+  document.getElementById ("McLaren").style.color = "white";
+  document.getElementById ("Lamborghini").style.color = "white";
+  document.getElementById ("Mercedes").style.color = "white";
+  document.getElementById ("BMW").style.color = "white";
+  document.getElementById ("Bugatti").style.color = "white";
+  document.getElementById ("Lancer").style.color = "white";
+  
+}
+
+function grayBugatti() {
+  document.getElementById ("McLaren").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lamborghini").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Mercedes").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("BMW").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Bugatti").style.color = "white";
+  document.getElementById ("Lancer").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("McLaren").style.transition = "1s"
+  document.getElementById ("Lamborghini").style.transition = "1s"
+  document.getElementById ("Mercedes").style.transition = "1s"
+  document.getElementById ("BMW").style.transition = "1s"
+  document.getElementById ("Bugatti").style.transition = "1s"
+  document.getElementById ("Lancer").style.transition = "1s"
+  
+}
+
+
+
+
+function levBugatti() {
+  document.getElementById ("McLaren").style.color = "white";
+  document.getElementById ("Lamborghini").style.color = "white";
+  document.getElementById ("Mercedes").style.color = "white";
+  document.getElementById ("BMW").style.color = "white";
+  document.getElementById ("Bugatti").style.color = "white";
+  document.getElementById ("Lancer").style.color = "white";
+  
+}
+
+function grayLancer() {
+  document.getElementById ("McLaren").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lamborghini").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Mercedes").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("BMW").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Bugatti").style.color = "rgb(93, 90, 90)";
+  document.getElementById ("Lancer").style.color = "white";
+  document.getElementById ("McLaren").style.transition = "1s"
+  document.getElementById ("Lamborghini").style.transition = "1s"
+  document.getElementById ("Mercedes").style.transition = "1s"
+  document.getElementById ("BMW").style.transition = "1s"
+  document.getElementById ("Bugatti").style.transition = "1s"
+  document.getElementById ("Lancer").style.transition = "1s"
+  
+}
+
+
+
+
+function levLancer() {
+  document.getElementById ("McLaren").style.color = "white";
+  document.getElementById ("Lamborghini").style.color = "white";
+  document.getElementById ("Mercedes").style.color = "white";
+  document.getElementById ("BMW").style.color = "white";
+  document.getElementById ("Bugatti").style.color = "white";
+  document.getElementById ("Lancer").style.color = "white";
+  
+}
+
+// end navs.............................................
+
+// onclick..............................................
+var logo = document.getElementById ("logo");
+var button1 = document.getElementById ("McLaren");
+var button2 = document.getElementById ("Lamborghini");
+var button3 = document.getElementById ("Mercedes");
+var button4 = document.getElementById ("BMW");   
+var button5 = document.getElementById ("Bugatti");   
+var button6 = document.getElementById ("Lancer");   
+
+
+logo.onclick = function() { 
+
+  window.scrollTo({
+    top : 0,
+    behavior : "smooth",
+  })
+}
+
+button1.onclick = function() {
+
+  window.scrollTo({
+    top : 870,
+    behavior : "smooth", 
+  });
+
+
+};
+
+button2.onclick = function() {
+
+  window.scrollTo({
+    top : 1580,
+    behavior : "smooth", 
+  });
+
+
+};
+button3.onclick = function() {
+
+  window.scrollTo({
+    top : 2279,
+    behavior : "smooth", 
+  });
+
+
+};
+
+button4.onclick = function() {
+
+  window.scrollTo({
+    top : 2990,
+    behavior : "smooth", 
+  });
+
+
+};
+
+button5.onclick = function() {
+
+  window.scrollTo({
+    top : 3690,
+    behavior : "smooth", 
+  });
+
+
+};
+
+button6.onclick = function() {
+
+  window.scrollTo({
+    top : 4375,
+    behavior : "smooth", 
+  });
+
+
+};
+
+// end onclick..............................................
+
+
+
+
